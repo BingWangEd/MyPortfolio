184484f86655051e3c70690162553628c3b3f0df
Bing Wang <bwang01@uchicago.edu>
2019-02-15 08:21:13 -0600

    added on styling

--- client/src/containers/HomeImage.js
+++ client/src/containers/HomeImage.js
@@ -6,19 +6,49 @@ const styles = {
   slide: {
     zIndex: '2',
     position: 'relative',
-    padding: '25vh 10%',
+    
     minHeight: '100vh',
     width: '100vw',
     boxSizing: 'border-box',
     boxShadow: '0 -1px 10px rgba(0, 0, 0, .7)',
-    transformStyle: 'inherit'
+    backgroundRepeat: 'no-repeat', 
+    backgroundSize: '100%',
+    backgroundPosition: 'center',
+    position:'relative'
+  },
+  shade: {
+    padding: '25vh 10%',
+    backgroundColor: 'rgba(50, 50, 50, 0.6)',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    minHeight: 'inherit'
+  },
+  quote: {
+    width: '50%',
+    paddingLeft: '35%',
+    color: 'white'
+  },
+  header: {
+    paddingBottom: '30px'
+  },
+  intro: {
+    borderLeft: '4px solid #FF634D',
+    paddingLeft: '15px'
   }
 };
 
 const HomeImage = () => (
   <div style={[styles.slide,  {backgroundImage: "url('"+ process.env.PUBLIC_URL +"Images/Teaching.jpg')"}]} className='slide'>
-    <div>
-      <h1>I'm aspired to build and design the best learning experience for students.</h1>
+    <div style={[styles.shade]}>
+      <div style={[styles.quote]}>
+        <h1 style={[styles.header]}>I aspire to develop the best learning experience for students.</h1>
+        <div style={[styles.intro]}>
+          <p>Bing Wang</p>
+          <p>EdTech Developer</p>
+        </div>
+      </div>
     </div>
   </div>
 )
--- client/src/containers/Timeline/ExperienceItem.js
+++ client/src/containers/Timeline/ExperienceItem.js
@@ -4,7 +4,8 @@ import './Experience.css';
 
 const styles = {
   slide: {
-    backgroundImage: 'url(\'https://lorempixel.com/640/480/abstract/6/\')',
+    backgroundColor: 'rgba(50, 50, 50)',
+    color: 'white',
     zIndex: '2',
     position: 'relative',
     padding: '25vh 10%',
@@ -12,16 +13,28 @@ const styles = {
     width: '100vw',
     boxSizing: 'border-box',
     boxShadow: '0 -1px 10px rgba(0, 0, 0, .7)',
-    transformStyle: 'inherit'
+    transformStyle: 'inherit',
+    position: 'relative'
+  },
+  words: {
+    width: '60%',
+    paddingLeft: '35%',
+    color: 'white'
+  },
+  category: {
+    fontSize: '600%',
+    position: 'absolute',
+    opacity: '0.1'
   }
 };
 
 const ExperienceItem = (experience) => (
   <div style={[styles.slide]} className='slide'>
-    <div>
+    <div style={[styles.words]}>
       <h3>{experience.experience.organization.name}</h3>
       <h1>{experience.experience.story}</h1>
     </div>
+    <div style={[styles.category]}><h1>{experience.experience.category}</h1></div>
   </div>
 )
 
